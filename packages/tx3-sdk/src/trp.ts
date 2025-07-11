e428d16b597d9c811a8af554bab9bb3501a1265f
Santiago Carmuega <santiago@carmuega.me>
2025-07-11 11:51:40 -0300

    feat: support TRP submit calls (#13)

--- packages/tx3-sdk/src/trp.ts
+++ packages/tx3-sdk/src/trp.ts
@@ -4,6 +4,11 @@ type Args = {
   [key: string]: ArgValue;
 };
 
+export type BytesEnvelope = {
+  content: string;
+  encoding: "base64" | "hex" | string;
+};
+
 export type TirEnvelope = {
   version: string;
   bytecode: string;
@@ -15,10 +20,22 @@ export type ProtoTx = {
   args: Args;
 };
 
-export type TxEnvelope = {
+export type ResolveResponse = {
   tx: string;
-  bytes: string;
-  encoding: "base64" | "hex" | string;
+  hash: string;
+};
+
+export type VKeyWitness = {
+  type: "vkey";
+  key: BytesEnvelope;
+  signature: BytesEnvelope;
+};
+
+export type SubmitWitness = VKeyWitness;
+
+export type SubmitParams = {
+  tx: BytesEnvelope;
+  witnesses: SubmitWitness[];
 };
 
 export type ClientOptions = {
@@ -37,7 +54,7 @@ export class Client {
   private handleArgs(args: Args): Object {
     const trpValues = Object.fromEntries(
       Object.entries(args).map(([key, value]) => {
-        const trpKey = key.replace(/([a-z0-9])([A-Z])/g, '$1_$2').toLowerCase();
+        const trpKey = key.replace(/([a-z0-9])([A-Z])/g, "$1_$2").toLowerCase();
 
         if (value instanceof Uint8Array) {
           return [trpKey, `0x${Buffer.from(value).toString("hex")}`];
@@ -47,10 +64,10 @@ export class Client {
       })
     );
 
-    return trpValues
+    return trpValues;
   }
 
-  async resolve(protoTx: ProtoTx): Promise<TxEnvelope> {
+  async resolve(protoTx: ProtoTx): Promise<ResolveResponse> {
     const response = await fetch(this.options.endpoint, {
       method: "POST",
       headers: {
@@ -76,9 +93,39 @@ export class Client {
     const result = await response.json();
 
     if (result.error) {
-      throw new Error(`JSON-RPC error: ${result.error.message}`, { cause: result.error.data });
+      throw new Error(`JSON-RPC error: ${result.error.message}`, {
+        cause: result.error.data,
+      });
     }
 
-    return result.result as TxEnvelope;
+    return result.result as ResolveResponse;
+  }
+
+  async submit(params: SubmitParams): Promise<void> {
+    const response = await fetch(this.options.endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        ...this.options.headers,
+      },
+      body: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "trp.submit",
+        params,
+        id: crypto.randomUUID(),
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to resolve transaction: ${response.statusText}`);
+    }
+
+    const result = await response.json();
+
+    if (result.error) {
+      throw new Error(`JSON-RPC error: ${result.error.message}`, {
+        cause: result.error.data,
+      });
+    }
   }
 }
